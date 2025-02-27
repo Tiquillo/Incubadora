60902e6bd5120f0ffcdbf6be2b11885fa5027f69
tiquillo <ldelgado@estudiantec.cr>
2025-02-27 08:36:30 -0600

    first commit

---
+++ Incubadora/Incubadora.ino
@@ -0,0 +1,28 @@
+// Disipark AtTiny85
+// Luis Delgado
+// Incubadora de huevos
+// 10/2/2025
+
+//#include "DigiKeyboard.h"
+
+int bombillo = 1;
+int termometro = A1;
+
+void setup() {
+  pinMode(bombillo, OUTPUT);
+  pinMode(termometro, INPUT);
+}
+
+void loop() {
+
+  // Para un divisor de tensión perfecto se usa 640 como referencia a 37 grados
+
+  int lectura = analogRead(termometro);
+  //DigiKeyboard.println(lectura);
+  if(lectura < 345){
+    digitalWrite(bombillo, LOW);
+  }else if (lectura > 370){
+    digitalWrite(bombillo, HIGH);
+  }
+  //delay(1000);
+}
